c8f9d3c13d78933b8adb215da8586a205d198aba
leomano10 <manologabiraman77@gmail.com>
2026-03-10 13:31:26 +0530

    success

--- mano/myapp/views.py
+++ mano/myapp/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
-def calculate_bill(request):
-    bill = 0
 
+def gst_amt_calculator(request):
+    gst_amt = None
     if request.method == 'POST':
-        p = float(request.POST.get('Price', 0))
-        gst = float(request.POST.get('GST', 0))
-        bill = p + (p * gst / 100)
+        try:
+            price = float(request.POST.get('price'))
+            gst = float(request.POST.get('gst'))
+           
 
-        print("Price =", p)
-        print("GST =", gst)
-        print("Total Bill =", bill)
+            if price >= 0 and gst >= 0:
+                gst_amt = price + ((price * gst) / 100)
+            else:
+                # Handle potential negative inputs error
+                gst_amt = "Error: Inputs cannot be negative."
+        except ValueError:
+            # Handle potential non-numeric inputs error
+            gst_amt = "Error: Invalid input. Please enter numeric values."
 
-    return render(request, 'myapp/math.html', {'bill': bill})
+    # Pass the result to the template
+    context = {
+        'gst_amt': gst_amt
+    }
+    return render(request,'math.html',context)
